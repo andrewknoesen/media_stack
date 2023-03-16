2055f9233e5fd70aeceb4d8d871bc7022751ece0
Andrew Knoesen <andrew@spatialedge.co.za>
2023-03-16 16:13:10 +0200

    Initial commit

---
+++ docker_images/sonarr/build_run_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir /Users/andrew/arr
+mkdir /Users/andrew/arr/sonarr
+mkdir /Users/andrew/arr/sonarr/config
+mkdir /Users/andrew/arr/media/series
+mkdir /Users/andrew/arr/media/downloads
+chmod -R 777 /Users/andrew/arr/
+
+docker compose up

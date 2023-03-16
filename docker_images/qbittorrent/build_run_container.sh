7ceadcf8ccd20dd2865da1216e32f03709bcd786
Andrew Knoesen <andrew@spatialedge.co.za>
2023-03-16 16:45:44 +0200

    Initial commit

---
+++ docker_images/qbittorrent/build_run_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir /Users/andrew/arr
+mkdir /Users/andrew/arr/qbittorrent
+mkdir /Users/andrew/arr/qbittorrent/config
+mkdir /Users/andrew/arr/media/downloads
+chmod -R 777 /Users/andrew/arr/
+
+docker compose up -d

929e3edf3b5dac149ae7fa20f80b7cd78e2df167
Andrew Knoesen <andrew@spatialedge.co.za>
2023-03-16 16:10:08 +0200

    ADD: Creating and chmoding of folders. Folders mapped in docker compose

--- docker_images/radarr/build_run_container.sh
+++ docker_images/radarr/build_run_container.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+mkdir /Users/andrew/arr
+mkdir /Users/andrew/arr/radarr
+mkdir /Users/andrew/arr/radarr/config
+mkdir /Users/andrew/arr/media/movies
+mkdir /Users/andrew/arr/media/downloads
+chmod -R 777 /Users/andrew/arr/
+
 docker compose up
